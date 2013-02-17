5a3612bb48534057d2d8a5d9101fb5b0b111e0e1
J. Félix Ontañón <fontanon@emergya.es>
2013-02-17 02:33:01 +0100

    Analysis completed

--- dataanalysys-project1/src/reproducible_code.R
+++ dataanalysys-project1/src/reproducible_code.R
@@ -23,8 +23,8 @@ cleanLoansData$Employment.Length <- as.numeric(loansData$Employment.Length)
 
 # Interest Rate vs. FICO-Range Analysis
 IRFR.lm <- lm(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$FICO.Range))
-IRFR.rsquared <- summary(IRFR.lm)$r.squared
-IRFR.confint <- confint(IRFR.lm)
+summary(IRFR.lm)$r.squared
+confint(IRFR.lm)
 
 # Plot Interest Rate vs. FICO Range
 par(mfrow=c(1,2))
@@ -47,18 +47,43 @@ summary(lm(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$Loan.Length)
 summary(lm(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$Loan.Purpose)))$r.squared
 summary(lm(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$Amount.Requested)))$r.squared
 
+
 # Interest Rate vs. Loan Length analysis
 IRLL.lm <- lm(cleanLoansData$Interest.Rate ~ as.factor(cleanLoansData$Loan.Length))
-IRLL.rsquared <- summary(lm(cleanLoansData$Interest.Rate ~ cleanLoansData$Loan.Length))$r.squared
-IRLL.confint <- confint(IRLL.lm)
+summary(lm(cleanLoansData$Interest.Rate ~ cleanLoansData$Loan.Length))
+confint(IRLL.lm)
 
 par(mfrow=c(1,2))
 plot(cleanLoansData$Interest.Rate ~ as.factor(cleanLoansData$Loan.Length), varwidth=TRUE)
 
 # Interest Rate vs. Amount Requested analysis
 IRAR.lm <- lm(cleanLoansData$Interest.Rate ~ cleanLoansData$Amount.Requested)
-IRAR.rsquared <- summary(lm(cleanLoansData$Interest.Rate ~ cleanLoansData$Amount.Requested))$r.squared
-IRAR.confint <- confint(IRAR.lm)
+summary(lm(cleanLoansData$Interest.Rate ~ cleanLoansData$Loan.Length))
+confint(IRAR.lm)
 
 smoothScatter(cleanLoansData$Amount.Requested, cleanLoansData$Interest.Rate, cex=5, col="blue")
 lines(cleanLoansData$Amount.Requested, IRAR.lm$fitted.values, lwd=5, col="red")
+
+# Interest Rate vs. FICO Range + Loan Length
+IRFRLL.lm <- lm(cleanLoansData$Interest.Rate ~ cleanLoansData$FICO.Range + as.factor(cleanLoansData$Loan.Length))
+IRFRLL36.lm <- lm(cleanLoansData$Interest.Rate[cleanLoansData$Loan.Length==36] ~ as.numeric(cleanLoansData$FICO.Range[cleanLoansData$Loan.Length==36]))
+IRFRLL60.lm <- lm(cleanLoansData$Interest.Rate[cleanLoansData$Loan.Length==60] ~ as.numeric(cleanLoansData$FICO.Range[cleanLoansData$Loan.Length==60]))
+
+par(mfrow=c(1,1))
+plot(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$FICO.Range), col=as.factor(cleanLoansData$Loan.Length))
+IRFRLL.lm <- lm(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$FICO.Range) + as.factor(cleanLoansData$Loan.Length))
+lines(cleanLoansData$FICO.Range[cleanLoansData$Loan.Length==36], IRFRLL36.lm$fitted,col="black",lwd=3)
+lines(cleanLoansData$FICO.Range[cleanLoansData$Loan.Length==60], IRFRLL60.lm$fitted,col="red",lwd=3)
+
+# Interest Rate vs. FICO Range + (Amount Requested / Loan Length) inference analysis
+IRFRLLAR.lm <- lm(cleanLoansData$Interest.Rate ~ as.numeric(cleanLoansData$FICO.Range) + cleanLoansData$Amount.Requested/cleanLoansData$Loan.Length)
+summary(IRFRLLAR.lm)
+confint(IRFRLLAR.lm)
+
+library("lattice")
+amount.requested <- equal.count(cleanLoansData$Amount.Requested)
+xyplot(cleanLoansData$Interest.Rate ~ cleanLoansData$FICO.Range | amount.requested + as.factor(cleanLoansData$Loan.Length), panel = function(x,y, ...) {
+  panel.xyplot(x,y,...)
+  fit<-lm(y~as.numeric(x))
+  panel.abline(fit)
+}, xlab="FICO Range (score)", ylab="Interest Rate (%)", main = "Interest Rate vs. FICO Range")
